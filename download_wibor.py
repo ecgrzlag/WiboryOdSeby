9c04450f7c77f27aefdd53990b2a6a947f9b8c64
ecgrzlag <grzegorz.lagozny@eurocash.pl>
2026-09-14 13:07:55 +0200

    Update POLSTR mappings in download_wibor.py
    Added additional POLSTR entries for different maturities.

--- download_wibor.py
+++ download_wibor.py
@@ -10,7 +10,11 @@ TICKERS = {
     'plopln6m': 'WIBOR_6M',
     'ploplnon': 'WIBOR_ON',
     'plbplnon': 'WIBID_ON',
-    'plspln1m': 'POLSTR_1M'
+    'plspln00': 'POLSTR',
+    'plspln1m': 'POLSTR_1M',
+    'plspln3m': 'POLSTR_3M',
+    'plspln6m': 'POLSTR_6M',
+    'plsplnci': 'POLSTR_CI'
 }
 
 def download_single_ticker(page, ticker, name, download_dir):
